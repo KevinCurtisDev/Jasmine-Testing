3ab47364c34601f6e6b00579f14e1f2cef93505b
kevin <kpcurtis@gmail.com>
2018-09-04 11:15:36 +0100

    Refactoredaddition function and jasmin addition test

--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -1,7 +1,27 @@
 describe("Calculator", function() {
-	describe("Addition function", function() {
-		it("should fail", function() {
-			expect(addition()).toBe(42);
+
+	beforeEach(function() {
+		calc = new Calculator();
+	});
+
+	describe("Add method", function() {
+		it("should take two arguments and return the sum of those arguments", function() {
+			calc.add(2);
+			calc.add(2);
+			expect(calc.value).toBe(4);
 		});
-	})
-})
+
+
+		it("should not return 4 if the sum of the arguments given don't equal 4", function() {
+			calc.add(7);
+			calc.add(19);
+			expect(calc.value).toBe(26);
+		});
+
+		it("should have called the alert function if either argument is undefined", function(){
+			spyOn(window, "alert");
+			calc.add("Hello");
+			expect(window.alert).toHaveBeenCalledWith("Argument must be a number");
+		});
+	});
+});
